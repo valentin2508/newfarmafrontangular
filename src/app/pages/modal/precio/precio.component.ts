94b1b7b92fc11c709f3df06ee483b22999324d7f
jose <josevalentin250890@gmail.com>
2025-06-24 17:57:26 -0500

    addProdsToModals

--- src/app/pages/modal/precio/precio.component.ts
+++ src/app/pages/modal/precio/precio.component.ts
@@ -2,7 +2,7 @@
 import { Component, Inject, inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatDialogRef } from '@angular/material/dialog';
-import { FormGroup, FormControl, FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 
@@ -16,17 +16,8 @@ import { CommonModule } from '@angular/common';
 export class PrecioComponent implements OnInit{
 
  // constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
- totalVenta: number = 0;
-cantidad: number = 1;
-  formPrecios: FormGroup | undefined; // Declara el FormGroup
-constructor(
-    public dialogRef: MatDialogRef<PrecioComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: PrecioComponent, // Inyecta los datos
-   // private fb: FormBuilder // Inyecta FormBuilder
-  ) {}
-private fb = inject(FormBuilder);
-
-
+  totalVenta: number = 0;
+  cantidad: number = 1;
   nombre: string = '';
   stock: number = 0;
   cantidadLlevar: number = 0;
@@ -34,32 +25,41 @@ private fb = inject(FormBuilder);
   precioBlister: number = 0;
   precioCaja: number = 0;
 
-  PU:string='';
-  PB:string='';
-  PC:string='';
-
-  ngOnInit(): void {
-    this.llenarModal();    
+  
+  formPrecios: FormGroup | any; // Declara el FormGroup
+constructor(
+    public dialogRef: MatDialogRef<PrecioComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any, 
+    private fb: FormBuilder 
+  ) {
     
   }
- form=this.fb.group({
-    precioVenta:this.data.precioVenta,
-    precioBlister:this.data.precioBlister,
-    precioCaja:this.data.precioCaja,
-    PU:"PU",
-    PB:"PB",
-    PC:"PC",
-   });
+  ngOnInit(): void {
+  
+    this.llenarModal();
+    this.formPrecios = this.fb.group({
+      nombre: [this.nombre],
+      stock: [this.stock],
+      precioVenta: [this.precioVenta],
+      precioBlister: [this.precioBlister],
+      precioCaja: [this.precioCaja],
+      PU: ['PU'],
+      PB: ['PB'],
+      PC: ['PC']
+    });
+    
+  }
 
     private llenarModal(): void 
     {
-    this.nombre=this.data.nombre;
-    this.stock=this.data.stock;
-    //this.precioVenta=this.data.precioVenta;
-    //this.precioBlister=this.data.precioBlister;
-    //this.precioCaja=this.data.precioCaja;
+    this.nombre = this.data.producto ;
+    this.stock=this.data.Stock;
+
+    this.precioVenta=this.data.precioVenta;
+    this.precioBlister=this.data.precioBlister;
+    this.precioCaja=this.data.precioCaja;
     this.cantidadLlevar=this.data.cantidadLlevar;
-    console.log("nnnnnnnnnnnnnnnnn",this.data);
+     console.log("Datos recibidos en llenarModal:", this.nombre);
     }
 
   seleccionarPrecio() {
